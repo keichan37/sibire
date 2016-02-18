d8b1646ed26d507ebedfc163383412a70ed21a9c
Kei Abukawa <Kei@Keis-Mac-mini.local>
2016-02-18 09:29:30 +0900

    heroku用

--- wp-config.php
+++ wp-config.php
@@ -17,26 +17,32 @@
  *
  * @package WordPress
  */
- 
+
 // 注意: 
 // Windows の "メモ帳" でこのファイルを編集しないでください !
 // 問題なく使えるテキストエディタ
 // (http://wpdocs.sourceforge.jp/Codex:%E8%AB%87%E8%A9%B1%E5%AE%A4 参照)
 // を使用し、必ず UTF-8 の BOM なし (UTF-8N) で保存してください。
-//mysql://bf8a0824b6b17c:e2e02e66@us-cdbr-iron-east-03.cleardb.net/heroku_07dc9a4cdf10ea5?reconnect=true
+
 // ** MySQL 設定 - この情報はホスティング先から入手してください。 ** //
 /** WordPress のためのデータベース名 */
-define('DB_NAME', 'heroku_07dc9a4cdf10ea5');
+define('DB_NAME', 'heroku_15097336c6a4b99');
+
 /** MySQL データベースのユーザー名 */
-define('DB_USER', 'bf8a0824b6b17c');
+define('DB_USER', 'bccb3ccdad0838');
+
 /** MySQL データベースのパスワード */
-define('DB_PASSWORD', 'e2e02e66');
+define('DB_PASSWORD', 'ba676c2d');
+
 /** MySQL のホスト名 */
 define('DB_HOST', 'us-cdbr-iron-east-03.cleardb.net');
+
 /** データベースのテーブルを作成する際のデータベースの文字セット */
 define('DB_CHARSET', 'utf8mb4');
+
 /** データベースの照合順序 (ほとんどの場合変更する必要はありません) */
 define('DB_COLLATE', '');
+
 /**#@+
  * 認証用ユニークキー
  *
@@ -46,15 +52,17 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'qXI2+sy29b)h8QM6SSs-[lD2Vsk!{s,R88XT6hBc5ev][C}gkV[PH4awKnv|DOh3');
-define('SECURE_AUTH_KEY',  '&_R3CZ)#Lyq<:Zzxp?eHe&o.Lt=S19<(]uHy3oxcJ+ae.bn+lZ2#~ei{@{t3aDnV');
-define('LOGGED_IN_KEY',    'RN>R)PTp0]c;#_]XQ3!te{nE0T&_w[^:,/HB`fw~TP+kVtm2K>8qV+au.yDnfi3_');
-define('NONCE_KEY',        'g_F/*Zj-d/<:lXm^%cNl8Y?Te{9%?e,oPw^,N)|Y[!6(AR&a]fW_j`+LVa42+2KG');
-define('AUTH_SALT',        'H5XOeGsD7_^KoGP(lWMHI_U8f?2xbkN*{]xrf8+p~[:m,Q7j.9FNZ>BNTRX%=~F:');
-define('SECURE_AUTH_SALT', 'D]cy-x6NfG Nbut/gmxAU0`^P+Kleg({vLgxVEIn1Z?N&/}BQNGPfoVCUG*4Qf)+');
-define('LOGGED_IN_SALT',   'iV:Dec(S9g=-.~BKT^d7;sxNHm+HnM;`^le3*0X&,p}Mksg|xG@E!1dm`B|a@!n.');
-define('NONCE_SALT',       '4fIaIu2Q-Z44&Nk~=T/(bbm^uVjR~eH+|+~|HcUSW_49:7>tHQK--hb=PtU]P84F');
+define('AUTH_KEY',         '&{&YAuxs<lXLz!9&nfA3hW-wphseQz!q?&o;7R2~4Nw`np:&}OEw2C/< q+4]3xo');
+define('SECURE_AUTH_KEY',  'WA|q1jhjR;gvuK1Y|>%[a4Gc93+_#:geu!g$.%VSht+j|On+4K+_u%=u|eO|bVZR');
+define('LOGGED_IN_KEY',    '!JHZ~.Vm++ d.*Y|<Ym+hh)>[K,oVC=++O1qsr0lmh/je__v@g|2>^dWYKOXg4~U');
+define('NONCE_KEY',        'zjF3hhMW0M)ePyKTr]IQISHw[qa+5~>=eT|;5Yg8v7tboH;*8co*Y+Q<kGq;d)v*');
+define('AUTH_SALT',        '.dSy#q{Rf=t-z*`TM}X5L_LhD[v[6@MTk)S[<1t3%M -ia_JxB(V-~Z|=RC-4d-T');
+define('SECURE_AUTH_SALT', '8KJ;#|4|JvX~eLCW|t_z}u{ish8OS1`q4)Jr;!EYwiNzg|=:-9P&t_&oQE $<7{{');
+define('LOGGED_IN_SALT',   'p1>cu@^o,oGY_w%(Bw7v~[i=Zfg2aTM$h7xGz;^wmDZ!b*H8/SbHJ/bfn6r G&(O');
+define('NONCE_SALT',       'AWQJG5:c)8-c_L;}M?:=PEnc mR:#%kgG[(S4q1(7A0P%~r!h=aK-@%zciEHXsfw');
+
 /**#@-*/
+
 /**
  * WordPress データベーステーブルの接頭辞
  *
@@ -62,6 +70,7 @@ define('NONCE_SALT',       '4fIaIu2Q-Z44&Nk~=T/(bbm^uVjR~eH+|+~|HcUSW_49:7>tHQK-
  * インストールすることができます。半角英数字と下線のみを使用してください。
  */
 $table_prefix  = 'wp_';
+
 /**
  * 開発者へ: WordPress デバッグモード
  *
@@ -73,9 +82,13 @@ $table_prefix  = 'wp_';
  * @link http://wpdocs.osdn.jp/WordPress%E3%81%A7%E3%81%AE%E3%83%87%E3%83%90%E3%83%83%E3%82%B0
  */
 define('WP_DEBUG', false);
+
 /* 編集が必要なのはここまでです ! WordPress でブログをお楽しみください。 */
+
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
+
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
