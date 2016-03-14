5cd738c9248df38d96d92585dfe327602968149f
Kei Abukawa <Kei@Keis-Mac-mini.local>
2016-03-14 16:29:27 +0900

    画像リンク切れ解消

--- wp-content/themes/sibire_20160217/single-recruit.php
+++ wp-content/themes/sibire_20160217/single-recruit.php
@@ -20,7 +20,10 @@
                   <a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>" data-text="<?php the_title(); ?> (<? $txt = get_field('recruit'); if($txt){ ?><? echo $txt; ?> <? } ?>)" data-lang="ja" data-hashtags="シビレる">ツイート</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
                   <div class="fb-like" data-href="<?php the_permalink(); ?>" data-layout="button" data-action="like" data-show-faces="false" data-share="true"></div>
                 </div>
+                <?php/*
                 <div class="company-img"><?php the_post_thumbnail( 980, 0 ); ?></div>
+                */?>
+                <div class="company-img"><img src="/wp-content/uploads/2016/03/galileoscope.jpg"></div>
                 <div class="articleDate"><span><i class="fa fa-calendar"></i>
                 <ul class="tag">
                   <?php
