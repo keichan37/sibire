9d2d4803bf48d17351807a0ac85c6ebaf2c6b02f
虻川景 <Kei@Macintosh.local>
2016-11-17 13:49:42 +0900

    password

--- wp-content/themes/sibire/partials/area-map.php
+++ wp-content/themes/sibire/partials/area-map.php
@@ -5,6 +5,7 @@
        'post_type' => array('recruit','offer'),
        'meta_key' => 'area',
        'meta_value' => $area_name,
+       'has_password' => false,
        'meta_compare'=>'LIKE'
   ); ?>
 <?php query_posts( $args ); ?>
