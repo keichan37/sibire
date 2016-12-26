a6170df3f37c9ecf2288ff4640b3d1be08cc9521
虻川景 <Kei@Macintosh.local>
2016-12-26 13:48:46 +0900

    #post h1 css

--- wp-content/themes/sibire/page.php
+++ wp-content/themes/sibire/page.php
@@ -4,25 +4,27 @@ description: このファイルが基本フォーマットになる
 <?php */ ?>
 
   <?php get_header(); ?>
-        <div id="contents">
-          <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <div class="page-cover">
+        <div id="post" <?php body_class(); ?>>
+          <div id="contents">
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+              <div class="page-cover">
+                <div class="container">
+                  <h1><?php the_title(); ?><?php show_page_number(); ?></h1>
+                </div>
+              </div>
               <div class="container">
-                <h1><?php the_title(); ?><?php show_page_number(); ?></h1>
-              </div>
+                <div id="main-content">
+                  <?php get_template_part('breadcrumb'); ?>
+                  <?php the_content(); //本文 ?>
+                </div>
+                <div id="sidebar">
+                  <?php get_template_part('sidebar'); //サイドバー ?>
+                </div>
+                <div class="clear"></div>
+              <?php endwhile; else: ?>
+                <p>記事がありません</p>
+              <?php endif; ?>
             </div>
-            <div class="container">
-              <div id="main-content">
-                <?php get_template_part('breadcrumb'); ?>
-                <?php the_content(); //本文 ?>
-              </div>
-              <div id="sidebar">
-                <?php get_template_part('sidebar'); //サイドバー ?>
-              </div>
-              <div class="clear"></div>
-            <?php endwhile; else: ?>
-              <p>記事がありません</p>
-            <?php endif; ?>
           </div>
+          <?php get_footer(); ?>
         </div>
-        <?php get_footer(); ?>
