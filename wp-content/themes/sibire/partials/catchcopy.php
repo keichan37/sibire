42c6b78afa8d51c6ea93a1f362794453134d11f7
Kei Abukawa <keichan37@gmail.com>
2017-02-21 18:01:46 +0900

    catchcopy edit

--- wp-content/themes/sibire/partials/catchcopy.php
+++ wp-content/themes/sibire/partials/catchcopy.php
@@ -5,21 +5,20 @@
    'post_status' => 'publish',
    'has_password' => false,
   );
+  $my_query = new WP_Query( $args );  //　クエリーを新規作成
 ?>
-  <?php $my_query = new WP_Query( $args ); ?>
+<?php if ( $my_query->have_posts() ) : ?>
   <?php while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
 
-    <?php
-      $thumbnail_id = get_post_thumbnail_id();
-      $thumbnail_url = wp_get_attachment_image_src($thumbnail_id,'thumbnail-size', true);
-    ?>
     <?php the_title(); ?>
     <? $txt = get_field('catchcopy_link'); if($txt){ ?><? echo $txt; ?> <? } ?>
     <? $txt = get_field('catchcopy_twitter'); if($txt){ ?><? echo $txt; ?> <? } ?>
     <? $txt = get_field('catchcopy_area'); if($txt){ ?><? echo $txt; ?> <? } ?>
 
-  <?php endwhile; ?>  
-<?php 
-endforeach; 
-wp_reset_postdata();
-?>
+  <?php endwhile; ?>
+
+  <?php else : ?>
+    <p>Coming Soon…</p>
+  <?php endif; ?>
+
+<?php wp_reset_postdata(); ?>
