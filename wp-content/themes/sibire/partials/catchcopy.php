49c7ea83fb6d38da5f62985f252f5668645c173a
Kei Abukawa <keichan37@gmail.com>
2017-02-21 17:54:44 +0900

    サブループいけるのかな

--- wp-content/themes/sibire/partials/catchcopy.php
+++ wp-content/themes/sibire/partials/catchcopy.php
@@ -5,19 +5,20 @@
    'post_status' => 'publish',
    'has_password' => false,
   );
-$postslist = get_posts($args);
-foreach ($postslist as $post) : setup_postdata($post);
 ?>
+  <?php $my_query = new WP_Query( $args ); ?>
+  <?php while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
 
-  <?php
-    $thumbnail_id = get_post_thumbnail_id();
-    $thumbnail_url = wp_get_attachment_image_src($thumbnail_id,'thumbnail-size', true);
-  ?>
-  <?php the_title(); ?>
-  <? $txt = get_field('catchcopy_link'); if($txt){ ?><? echo $txt; ?> <? } ?>
-  <? $txt = get_field('catchcopy_twitter'); if($txt){ ?><? echo $txt; ?> <? } ?>
-  <? $txt = get_field('catchcopy_area'); if($txt){ ?><? echo $txt; ?> <? } ?>
-  
+    <?php
+      $thumbnail_id = get_post_thumbnail_id();
+      $thumbnail_url = wp_get_attachment_image_src($thumbnail_id,'thumbnail-size', true);
+    ?>
+    <?php the_title(); ?>
+    <? $txt = get_field('catchcopy_link'); if($txt){ ?><? echo $txt; ?> <? } ?>
+    <? $txt = get_field('catchcopy_twitter'); if($txt){ ?><? echo $txt; ?> <? } ?>
+    <? $txt = get_field('catchcopy_area'); if($txt){ ?><? echo $txt; ?> <? } ?>
+
+  <?php endwhile; ?>  
 <?php 
 endforeach; 
 wp_reset_postdata();
