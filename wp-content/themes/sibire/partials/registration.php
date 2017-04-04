cba10a622bfae6bfabd8115841fec73f4b1c7d10
Kei Abukawa <keichan37@gmail.com>
2017-04-04 09:40:26 +0900

    columnカテゴリに登録ボタン表示 文言修正

--- wp-content/themes/sibire/partials/registration.php
+++ wp-content/themes/sibire/partials/registration.php
@@ -1,6 +1,6 @@
 <div class="partials-registration-wrap">
   <a class="partials-registration" href="/registration">登録する</a>
   <p>
-    sibireに登録すると、<br />エンジニアの皆さんが希望する移住エリアの企業情報を提供します
+    sibireに登録すると、<br />エンジニアの皆さんが希望するエリアやスキルに合わせた企業を紹介します
   </p>
 </div>
