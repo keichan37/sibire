52ce1af84baa1fef52e4e5bab9f80fee18f34213
Kei Abukawa <keichan37@gmail.com>
2017-04-12 15:35:10 +0900

    template-lp

--- wp-content/themes/sibire/template-lp.php
+++ wp-content/themes/sibire/template-lp.php
@@ -1,16 +1,16 @@
-<?php /* Template Name: サービス紹介LP*/ ?>
+<?php /* Template Name: サービス紹介LP */ ?>
 
-  <?php get_header("renewal"); //ヘッダーリニューアル?>
+  <?php get_header("lp"); //ヘッダーリニューアル?>
     <div id="lp">
       <div class="lp-cover">
-        <h1><?php the_title(); ?><?php show_page_number(); ?><br /><img src="<?php echo get_template_directory_uri(); ?>/images/template-service/lp-offtokyo.png" alt="OFF TOKYO"></h1>
+        <h1><?php the_title(); ?><?php show_page_number(); ?><br /><img src="<?php echo get_template_directory_uri(); ?>/images/template-lp/lp-offtokyo.png" alt="OFF TOKYO"></h1>
       </div>
       <section>
         <div class="lp-title-wrap">
           <h2>sibire<b>3</b>つの特徴</h2>
         </div>
         <div class="lp-characteristic-wrap lp-characteristic-wrap-right first-child">
-          <div class="lp-characteristic-img"><img class="slide-left" src="<?php echo get_template_directory_uri(); ?>/images/template-service/lp-characteristic-first-image.png" alt=""></div>
+          <div class="lp-characteristic-img"><img class="slide-left" src="<?php echo get_template_directory_uri(); ?>/images/template-lp/lp-characteristic-first-image.png" alt=""></div>
           <div class="lp-characteristic-inner">
             <h3>東京以外のシビレる求人に出会える</h3>
             <p>東京だと探しやすい求人も、エリアの垣根を越えると、<br />
@@ -19,7 +19,7 @@
           </div>
         </div>
         <div class="lp-characteristic-wrap lp-characteristic-wrap-left second-child">
-          <div class="lp-characteristic-img"><img class="slide-right" src="<?php echo get_template_directory_uri(); ?>/images/template-service/lp-characteristic-second-image.png" alt=""></div>
+          <div class="lp-characteristic-img"><img class="slide-right" src="<?php echo get_template_directory_uri(); ?>/images/template-lp/lp-characteristic-second-image.png" alt=""></div>
           <div class="lp-characteristic-inner">
             <h3>キャリアを落とさず転職できる</h3>
             <p>OFF TOKYOして、キャリアダウンしたら意味がない。<br />
@@ -28,7 +28,7 @@
           </div>
         </div>
         <div class="lp-characteristic-wrap lp-characteristic-wrap-right last-child">
-          <div class="lp-characteristic-img"><img class="slide-left" src="<?php echo get_template_directory_uri(); ?>/images/template-service/lp-characteristic-last-image.png" alt=""></div>
+          <div class="lp-characteristic-img"><img class="slide-left" src="<?php echo get_template_directory_uri(); ?>/images/template-lp/lp-characteristic-last-image.png" alt=""></div>
           <div class="lp-characteristic-inner">
             <h3>スキマ時間で活動できる </h3>
             <p>対面はもちろん、メールやハングアウト、Skypeなど<br />
@@ -69,7 +69,7 @@
       <section>
         <div class="lp-message-wrap">
           <div class="slide-bottom">
-            <h3>sibireが目指す世界<br /><img src="<?php echo get_template_directory_uri(); ?>/images/template-service/lp-offtokyo.png" alt="OFF TOKYO"></h3>
+            <h3>sibireが目指す世界<br /><img src="<?php echo get_template_directory_uri(); ?>/images/template-lp/lp-offtokyo.png" alt="OFF TOKYO"></h3>
             <p>「東京」という枠にはまらず働く。<br />
                 それはきっと、今の時代、エンジニアだけが与えられる特権。 <br />
                 東京で就職したからといって、永遠に東京に住み続ける必要はない。 <br />
@@ -88,4 +88,4 @@
       </div>
     </div>
         
-    <?php get_footer("renewal"); //フッターリニューアル?>
+    <?php get_footer("lp"); //フッターリニューアル?>
