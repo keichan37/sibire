74fe509e3775c8798c4793be3f73d50f849e4045
Kei Abukawa <keichan37@gmail.com>
2017-04-13 10:46:47 +0900

    partials/service

--- wp-content/themes/sibire/partials/service.php
+++ wp-content/themes/sibire/partials/service.php
@@ -1,3 +1,9 @@
 <a class="service-registration" href="/service#form">サービスに申し込む</a>
-<div class="service-registration-window"><b class="caret"></b>エンジニアのための転職を支援</span></div>
-
+<div class="service-registration-window"><b class="caret"></b>
+<?php if ( in_array(get_post_type(), array('recruit')) ): //求人用?>
+  この求人にシビレた！
+<?php else: ?>
+  OFF TOKYOしたい<br />
+  エンジニアの転職を支援
+<?php endif; ?>
+</span></div>
