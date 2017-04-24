9d3b7215a901185b1bbe3c6269565e0ea14c68b0
Kei Abukawa <keichan37@gmail.com>
2017-04-24 12:29:18 +0900

    template-single

--- wp-content/themes/sibire/template-single.php
+++ wp-content/themes/sibire/template-single.php
@@ -1,10 +1,10 @@
 <?php /* Template Name: 投稿ページ */ ?>
 
-  <?php get_header("lp"); //ヘッダーリニューアル?>
+  <?php get_header("lp"); ?>
     <div id="common">
       <div class="container">
         <div class="single-wrap">
-          <?php get_template_part('breadcrumb'); //パンくずリスト ?>
+          <?php get_template_part('breadcrumb'); ?>
           <div class="single-left">
             <?php while(have_posts()): the_post(); ?>
               <article>
@@ -12,28 +12,27 @@
                 <time class="single-date" datetime="<?php the_time('c') ;?>"><?php the_time('Y.n.j') ;?></time>
                 <h1 class="single-title"><?php the_title(); ?></h1>
                 <? $txt = get_field('subtitle'); if($txt){ ?><h2 class="single-subtitle"><? echo $txt; ?></h2><? } ?>
+                <?php get_template_part('partials/sns-sidebar'); ?>
                 <?php
                   $thumbnail_id = get_post_thumbnail_id();
                   $thumbnail_url = wp_get_attachment_image_src($thumbnail_id,'large', true); //アイキャッチのURL取得
                 ?>
-                <?php if (has_post_thumbnail()): //アイキャッチある場合 ?>
+                <?php if (has_post_thumbnail()): ?>
                   <img class="single-eyecatch" src="<?php echo $thumbnail_url[0]; ?>">
-                <?php else: //アイキャッチない場合 ?>
+                <?php else: ?>
                   <img class="single-eyecatch" src="<?php echo get_template_directory_uri(); ?>/images/common/no-image-eyecatch.png">
                 <?php endif; ?>
                 <div class="single-content mce-content-body"><?php the_content(); //本文 ?></div>
               </article>
             <?php endwhile; ?>
-            <?php get_template_part('partials/registration'); ?>
           </div>
           <div class="single-right">
-            <aside>
-              <?php get_template_part('partials/service'); ?>
-            </aside>
+            <?php get_template_part('partials/sidebar'); ?>
           </div>
+          <?php get_template_part('partials/registration'); ?>
         </div>    
       </div>    
       
     </div>
     <?php get_template_part('partials/sns-footer'); ?>
-    <?php get_footer("lp"); //フッターリニューアル?>
+    <?php get_footer("lp"); ?>
