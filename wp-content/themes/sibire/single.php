f8e2e7cd35a302f85d7f7bc5468288269ab2c01c
Kei Abukawa <keichan37@gmail.com>
2017-05-01 10:40:06 +0900

    投稿ページ

--- wp-content/themes/sibire/single.php
+++ wp-content/themes/sibire/single.php
@@ -1,80 +1,60 @@
-<?php /* ?>
-title: 記事フォーマット
-description: このファイルが基本フォーマットになる
-<?php */ ?>
+<?php /* Template Name: 投稿ページ */ ?>
 
-  <?php get_header(); //ヘッダー ?>
-        <div id="post" <?php body_class(); ?>>
-          <div id="contents">
-            <div class="container">
-              <div id="main-content">
-
-                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); //投稿が存在する場合 ?>
-                  <?php get_template_part('breadcrumb'); //パンくずリスト ?>
-                  <time class="entry-date" datetime="<?php the_time('c') ;?>">
-                    <?php the_time('Y年n月j日') ;?>
-                  </time>
-                  <h1 class="post-h1"><?php the_title(); ?><?php show_page_number(); ?></h1>
-                  <div class="post-name">
-                    <? $txt = get_field('subtitle'); if($txt){ ?><? echo $txt; ?> <? } ?>
-                  </div>
-                  <?php if ( in_array(get_post_type(), array('niche')) ): //シビレるニッチ用?>
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/single-niche-banner.jpg">
-                    <?php if ( $page == 1 ) : // 1ページ目だけ表示 ?>
-                      <div class="single-niche-top">
-                        <div class="single-niche-top-image">
-                          <img src="<?php the_field('single-niche-top-image'); ?>" alt="">
-                        </div>
-                        <div class="single-niche-top-text">
-                          <? $txt = get_field('single-niche-top-text'); if($txt){ ?><? echo $txt; ?> <? } ?>
-                        </div>
-                        <div class="clear"></div>
-                      </div>
-                    <?php endif; ?>
-                  <?php elseif ( in_array(get_post_type(), array('creator')) ): //シビレるクリエイター用?>
-                    <div class="single-creator-top">
-                      <div class="single-creator-top-image">
-                        <img src="<?php the_field('single-creator-top-image'); ?>" alt="">
-                      </div>
-                      <div class="single-creator-top-text">
-                        <? $txt = get_field('single-creator-top-text'); if($txt){ ?><? echo $txt; ?> <? } ?>
-                      </div>
-                      <div class="clear"></div>
-                    </div>
-                  <?php elseif ( in_array(get_post_type(), array('media')) ): //メディア掲載用?>
-                  <?php else: //シビレるニッチで非表示 ?>
-                    <?php
-                      $thumbnail_id = get_post_thumbnail_id();
-                      $thumbnail_url = wp_get_attachment_image_src($thumbnail_id,'thumbnail-size', true); //アイキャッチのURL取得
-                    ?>
-                    <?php if (has_post_thumbnail()): //アイキャッチある場合 ?>
-                      <img class="post-eyecatch-img" src="<?php echo $thumbnail_url[0]; ?>">
-                    <?php else: //アイキャッチない場合 ?>
-                      <?php /* ?>
-                      <div class="post-img" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/no-image-2x.png);"></div>
-                      <?php */ ?>
-                    <?php endif; ?>
-                  <?php endif; ?>
-                  
-                  <?php the_content(); //本文 ?>
-                  <?php if ( in_array(get_post_type(), array('column')) ): //シビレる小ネタ用 他カテゴリーが決まったらtemplate側で場合分けする?>
-                    <?php get_template_part('partials/registration');?>
-                  <?php endif; ?>
-                <?php endwhile; else: //投稿が存在しない場合 ?>
-                  <p>記事がありません</p>
+  <?php get_header("lp"); ?>
+    <div id="common">
+      <div class="container">
+        <div class="single-wrap">
+          <?php get_template_part('breadcrumb'); ?>
+          <div class="single-left">
+            <?php while(have_posts()): the_post(); ?>
+              <article>
+                <span class="single-category"><?php echo esc_html(get_post_type_object($post->post_type)->label); ?></span>
+                <time class="single-date" datetime="<?php the_time('c') ;?>"><?php the_time('Y.n.j') ;?></time>
+                <h1 class="single-title"><?php the_title(); ?></h1>
+                <? $txt = get_field('subtitle'); if($txt){ ?><h2 class="single-subtitle"><? echo $txt; ?></h2><? } ?>
+                <?php get_template_part('partials/sns-share'); ?>
+                <?php
+                  $thumbnail_id = get_post_thumbnail_id();
+                  $thumbnail_url = wp_get_attachment_image_src($thumbnail_id,'large', true); //アイキャッチのURL取得
+                ?>
+                <?php if (has_post_thumbnail()): ?>
+                  <img class="single-eyecatch" src="<?php echo $thumbnail_url[0]; ?>">
+                <?php else: ?>
+                  <img class="single-eyecatch" src="<?php echo get_template_directory_uri(); ?>/images/common/no-image-eyecatch.png">
                 <?php endif; ?>
 
-                <?php get_template_part('partials/link_pages'); //ページング ?>
+                <!-- PRとインタビュー -->
+                <?php if ( in_array(get_post_type(), array('interview')) ): ?>
+                  <div class="single-top-box">
+                    <img src="<?php the_field('avatar'); ?>" alt="">
+                    <div class="single-top-box-text"><? $txt = get_field('profile'); if($txt){ ?><? echo $txt; ?> <? } ?></div>
+                  </div>
+                <?php elseif ( in_array(get_post_type(), array('niche')) ): ?>
+                  <?php if ( $page == 1 ) : // 1ページ目だけ表示 ?>
+                    <div class="single-top-box">
+                      <img src="<?php the_field('single-niche-top-image'); ?>" alt="">
+                      <div class="single-top-box-text"><? $txt = get_field('single-niche-top-text'); if($txt){ ?><? echo $txt; ?> <? } ?></div>
+                    </div>
+                  <?php endif; ?>
+                <?php endif; ?>
 
-              </div>
-              <div id="sidebar">
-                <?php get_template_part('sidebar'); //サイドバー ?>
-              </div>
-              <div class="clear"></div>
-              <?php get_template_part('partials/iframe');?>
-              <?php get_template_part('partials/other','list');?>
-            </div>
+                <div class="single-content mce-content-body">
+                  <?php the_content(); //本文 ?>
+                  <?php get_template_part('partials/link_pages'); //ページング ?>
+                  <?php get_template_part('partials/single-table'); ?>
+                </div>
+              </article>
+            <?php endwhile; ?>
           </div>
-        </div>
-        
-        <?php get_footer(); //フッター ?>
+          <div class="single-right">
+            <?php get_template_part('partials/sidebar'); ?>
+          </div>
+          <aside>
+            <?php get_template_part('partials/registration'); ?>
+            <?php get_template_part('partials/related-list'); ?>
+          </aside>
+        </div>    
+      </div>    
+      
+    </div>
+    <?php get_footer("lp"); ?>
