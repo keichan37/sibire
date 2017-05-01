e6a0c9b73b2a5ae31cdb6d6f8b7bf23815326f77
Kei Abukawa <keichan37@gmail.com>
2017-05-01 12:08:14 +0900

    404回避

--- wp-content/themes/sibire/front-page.php
+++ wp-content/themes/sibire/front-page.php
@@ -12,7 +12,7 @@
               $args = array(
                 'paged' => $paged,
                 'post_type' => array('recruit','interview','offer','column','event','niche','blog'),
-                'posts_per_page' => 1,
+                'posts_per_page' => 27,
                 'post_status' => 'publish',
                 'has_password' => false,
               ); ?>
