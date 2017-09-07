d00012d6fe65b18f29695f2146efa25cff90e0de
Kei Abukawa <keichan37@gmail.com>
2017-09-07 17:20:35 +0900

    category.php

--- wp-content/themes/sibire/category.php
+++ wp-content/themes/sibire/category.php
@@ -1,4 +1,4 @@
-<?php /* Template Name: カスタム投稿タイプ一覧ページ */ ?>
+<?php /* Template Name: カテゴリページ */ ?>
 
   <?php get_header(); ?>
     <div id="common">
@@ -9,36 +9,18 @@
           </div>
         </div>
         <div class="container">
-            <div class="common-left">
-              <div class="common-grid-wrap">
-                <?php if(have_posts()): while(have_posts()):the_post(); ?>
-                  <?php
-                    $args = array( 
-                     'post_type' => array(post,recruit,column,niche,interview,media,'blog'),
-                     'post_status' => 'publish',
-                     'has_password' => false,
-                     'posts_per_page' => -1
-                    );
-                  ?>
+          <div class="common-left">
+            <div class="common-grid-wrap">
+              <?php if(have_posts()): while(have_posts()):the_post(); ?>
+                <?php get_template_part('partials/common-grid'); ?>
 
-                  <?php
-                    $postslist = get_posts($args);
-                    foreach ($postslist as $post) : setup_postdata($post);
-                  ?>
-                  <?php get_template_part('partials/common-grid'); ?>
-                  <?php 
-                    endforeach; 
-                    wp_reset_postdata();
-                  ?>
-                  <?php if (function_exists("pagination")) {
-                    pagination($custom_query->max_num_pages);
-                  } ?>
-
-
-            <?php endwhile; else: //投稿が存在しない場合 ?>
-              <p>記事がありません</p>
-            <?php endif; ?>
-          </div>
+              <?php endwhile; else: //投稿が存在しない場合 ?>
+                <p>記事がありません</p>
+              <?php endif; ?>
+            </div>
+            <?php if (function_exists("pagination")) {
+              pagination($custom_query->max_num_pages);
+            } ?>
         </div>    
         <div class="common-right">
           <?php get_search_form(); ?>
