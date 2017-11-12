66132116380f4c5350ac99ec040491b7dd0355c7
Kei Abukawa <keichan37@gmail.com>
2017-11-13 08:35:02 +0900

    エベント終了

--- wp-content/themes/sibire/partials/template-event-button.php
+++ wp-content/themes/sibire/partials/template-event-button.php
@@ -1,5 +1,8 @@
 <div class="button-wrap">
   <strong class="reservation">事前予約はここから!</strong>
+  <?php /*
   <a class="button" href="entry">参加する</a>
+  */ ?>
+  <a class="button button-end" href="javascript:void(0);">本イベントは終了しました</a>
   <b>確実に入場いただくために事前予約をオススメしています</b>
 </div>
