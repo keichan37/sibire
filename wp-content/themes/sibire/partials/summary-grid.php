53e58ee9a029705aee3abb556e5156faa2db325c
Kei Abukawa <keichan37@gmail.com>
2017-12-01 14:25:31 +0900

    summary-grid-firstクラス付与

--- wp-content/themes/sibire/partials/summary-grid.php
+++ wp-content/themes/sibire/partials/summary-grid.php
@@ -2,7 +2,11 @@
   $thumbnail_id = get_post_thumbnail_id();
   $thumbnail_url = wp_get_attachment_image_src($thumbnail_id,'medium', true);
 ?>
-<a href=<?php echo get_permalink(); ?> class="summary-grid">
+<?php if ($counter <= 1): ?>
+  <a href=<?php echo get_permalink(); ?> class="summary-grid summary-grid-first">
+<?php else: ?>
+  <a href=<?php echo get_permalink(); ?> class="summary-grid">
+<?php endif; ?>
   <figure>
     <?php if (has_post_thumbnail()): ?>
       <img src="<?php echo $thumbnail_url[0]; ?>" />
@@ -12,7 +16,9 @@
     <figcaption>
       <h3><?php the_title(); ?></h3>
       <h4><?php echo nl2br(get_post_meta($post->ID, 'subtitle', true)); ?></h4>
-      <p><?php the_excerpt(); ?></p>
+      <?php if ($counter <= 1): ?>
+        <?php the_excerpt(); ?>
+      <?php endif; ?>
     </figcaption>
   </figure>
 </a>
