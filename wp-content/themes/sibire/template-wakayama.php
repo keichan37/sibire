13f0d327a31919968ce9ae6917891692abdafd93
Kei Abukawa <keichan37@gmail.com>
2018-01-10 12:25:43 +0900

    背景変更

--- wp-content/themes/sibire/template-wakayama.php
+++ wp-content/themes/sibire/template-wakayama.php
@@ -4,7 +4,6 @@
     <div id="template-wakayama">
       <div id="cover">
         <div class="container">
-          <img class="cover-image" src="<?php echo get_template_directory_uri(); ?>/images/template-wakayama/cover-image.jpg" alt="">
           <img class="cover-image" src="<?php echo get_template_directory_uri(); ?>/images/template-wakayama/cover-arch.png" alt="">
           <h1><img src="<?php echo get_template_directory_uri(); ?>/images/template-wakayama/logo.png" alt="<?php the_title(); ?>"/><time datetime="<?php the_time('c') ;?>"><?php the_time('Y年n月d日(D)'); ?></time></h1>
           <img src="<?php echo get_template_directory_uri(); ?>/images/template-wakayama/building.png" alt="モンスーンカフェ 恵比寿店"/>
@@ -22,20 +21,19 @@
         <h2>
           <img src="<?php echo get_template_directory_uri(); ?>/images/template-wakayama/h2-1.png" alt="ワークショップ「わかやま“十人十色のくらしぶり”」"/>
         </h2>
-        <p>和歌山県で活躍する10社10人がこの日のために一挙集結。東京ではできない和歌山らしい暮らしをアピールします。
-企業10社の特徴を知って、自分に合う企業に出会ってください。</p>
+        <p>和歌山県で活躍する企業の皆さんがこの日のために一挙集結。 東京ではできない和歌山らしい暮らしをアピールします。 各企業の特徴を知って、自分に合う企業に出会ってください。</p>
       </div>
       <div class="section section2">
         <h2>
           <img src="<?php echo get_template_directory_uri(); ?>/images/template-wakayama/h2-2.png" alt="リアルタイムVR企業訪問"/>
         </h2>
-        <p>VRゴーグルを覗くと、目の前には和歌山県の企業が！東京に居ながらリアルタイムに会社訪問を実現</p>
+        <p>VRゴーグルを覗くと、目の前には和歌山県の景色が。 東京に居ながらリアルタイムに会社訪問を実現</p>
       </div>
       <div class="section section2">
         <h2>
           <img src="<?php echo get_template_directory_uri(); ?>/images/template-wakayama/h2-3.png" alt="和歌山のおいしいを体験！"/>
         </h2>
-        <p>和歌山県が誇るおいしい地の物とお酒を召し上がれ。海の幸も山の幸も味わいながら和歌山ぐらしを体感しよう。</p>
+        <p>和歌山県が誇るおいしい地の物とお酒を召し上がれ。 海の幸も山の幸も味わいながら和歌山ぐらしを体感しよう。</p>
       </div>
       <div class="section map" id="map">
         <?php get_template_part('google_map');?>
