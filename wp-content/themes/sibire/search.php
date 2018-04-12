cd565268a617d971c088077b2856edf0798bfc3c
Kei Abukawa <keichan37@gmail.com>
2018-04-12 16:21:21 +0900

    search

--- wp-content/themes/sibire/search.php
+++ wp-content/themes/sibire/search.php
@@ -4,75 +4,51 @@ description: 検索結果が表示されます
 <?php */ ?>
 
   <?php get_header(); //ヘッダー ?>
+    
+    <div id="summary">
+      <div id="search">
 
-    <div id="common">
-      <div class="common-cover tag">
+        <?php
+          $searchCat = $_GET['cat'];
+          $searctTag = $_GET['tag'];
+         
+          $searchCatObj = get_term_by( 'ID', $searchCat, 'category');
+          $searchTagObj = get_term_by( 'slug', $searctTag, 'post_tag');
+         
+          $searchCatStr = $searchCatObj->name;
+          $searchTagStr = $searchTagObj->name;
+         
+          $found_cnt = $wp_query->post_count;
+        ?>
+
         <div class="container">
-          <div class="breadcrumb">
-            <span itemscope itemtype="http://data-vocabulary.org/Breadcrumb">
-              <a href="<?php echo home_url(); ?>" itemprop="url">
-                <span itemprop="title">TOP</span>
-              </a>&gt; 
-            </span>
-            <span itemscope itemtype="http://data-vocabulary.org/Breadcrumb">
-              <span itemprop="title"><?php the_search_query(); ?></strong>の検索結果</span>
-            </span>
-          </div>
-
-          <?php
-            $searchCat = $_GET['cat'];
-            $searctTag = $_GET['tag'];
-           
-            $searchCatObj = get_term_by( 'ID', $searchCat, 'category');
-            $searchTagObj = get_term_by( 'slug', $searctTag, 'post_tag');
-           
-            $searchCatStr = $searchCatObj->name;
-            $searchTagStr = $searchTagObj->name;
-           
-            $found_cnt = $wp_query->post_count;
-          ?>
-
-          <?php if ($searchCatObj) echo 'カテゴリー: '.$searchCatStr; ?>
-          <?php if ($searchTagObj) echo 'タグ: '.$searchTagStr; ?>
-            <?php if (isset($_GET['s']) && empty($_GET['s'])) { ?>
-            <?php } else { ?>
-              <h1>“<strong><?php the_search_query(); ?></strong>”の検索結果</h1>
-            <?php } ?>
-          <div class="post-name">
-          </div>
-
-        </div>
-      </div>
-      <div class="container">
-        <div class="common-left">
-          <div class="common-grid-wrap">
-
-            <?php if (isset($_GET['s']) && empty($_GET['s'])) { ?>
-              <h2>検索条件が入力されていません。</h2>
-            <?php } else { ?>
-            <?php if(have_posts()) : ?>
-              <?php while(have_posts()):the_post() ?>
-                <?php get_template_part('partials/common-grid'); ?>
-              <?php endwhile; ?>
-              <?php if (function_exists("pagination")) {
-                pagination($custom_query->max_num_pages);
-              } ?>
-             <?php else : ?>
-              <h2>検索条件にヒットした記事がありませんでした。</h2>
-            <?php endif; ?> </ul>
-          <?php } ?>
+          <div class="page-wrp">
+            <?php if ($searchCatObj) echo 'カテゴリー: '.$searchCatStr; ?>
+            <?php if ($searchTagObj) echo 'タグ: '.$searchTagStr; ?>
+              <?php if (isset($_GET['s']) && empty($_GET['s'])) { ?>
+              <?php } else { ?>
+                <h1>検索結果: <strong><?php the_search_query(); ?></strong></h1>
+                <i><?php echo $wp_query->found_posts; ?>件</i>
+              <?php } ?>
 
 
-          </div>
+                <?php if (isset($_GET['s']) && empty($_GET['s'])) { ?>
+                  <h1>検索条件が入力されていません。</h1>
+                  <?php get_search_form(); ?>
+                <?php } else { ?>
+                <?php if(have_posts()) : ?>
+                  <?php get_template_part('partials/post-table'); ?>
+                  <?php if (function_exists("pagination")) {
+                    pagination($custom_query->max_num_pages);
+                  } ?>
+                 <?php else : ?>
+                  <i>検索条件にヒットした記事がありませんでした。</i>
+                  <?php get_search_form(); ?>
+                <?php endif; ?>
+              <?php } ?>
+
+          </div>    
         </div>    
-        <div class="common-right">
-          <?php get_search_form(); ?>
-          <?php /* 
-          <?php get_template_part('partials/recruit-map'); ?>
-          <?php get_template_part('partials/tag'); ?>
-          */ ?>
-          <?php get_template_part('partials/registration'); ?>
-        </div>
       </div>    
       
     </div>    
