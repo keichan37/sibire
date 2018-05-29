dde3563135e52774f57c5447df7652e7765f4785
Kei Abukawa <keichan37@gmail.com>
2018-05-29 15:59:47 +0900

    custome-field

--- wp-content/themes/sibire/template-custom-field.php
+++ wp-content/themes/sibire/template-custom-field.php
@@ -21,7 +21,7 @@
             $paged = get_query_var('paged') ? get_query_var('paged') : 1 ;
             $args = array(
               'paged' => $paged,
-              'posts_per_page' => -1,
+              'posts_per_page' => 12,
               'post_type' => $cat_slug,
               'post_status' => 'publish',
               'has_password' => false
@@ -32,14 +32,45 @@
             foreach ($postslist as $post) : setup_postdata($post);
           ?>
             <?php get_template_part('partials/summary-grid'); ?>
-          <?php 
-            endforeach; 
-            wp_reset_postdata();
-          ?>
-          <?php if (function_exists("pagination")) {
-            pagination($custom_query->max_num_pages);
-          } ?>
+          <?php endforeach; ?>
       </div>    
+
+      <nav class="paginate-nav">
+        <?php global $wp_rewrite;
+          $total_posts_num = get_posts(
+            array(
+              'paged' => $paged,
+              'posts_per_page' => -1,
+              'post_type' => $cat_slug,
+              'post_status' => 'publish',
+              'has_password' => false
+            )
+          );
+          $max_posts_num = count($total_posts_num);
+          $max_pages = ceil($max_posts_num/12);
+
+          $paginate_base = get_pagenum_link(1);
+          if(strpos($paginate_base, '?') || ! $wp_rewrite->using_permalinks()){
+            $paginate_format = '';
+            $paginate_base = add_query_arg('paged','%#%');
+          }
+          else{
+            $paginate_format = (substr($paginate_base,-1,1) == '/' ? '' : '/') .
+            user_trailingslashit('page/%#%/','paged');;
+            $paginate_base .= '%_%';
+          }
+          echo paginate_links(array(
+            'base' => $paginate_base,
+            'format' => $paginate_format,
+            'total' => $max_pages,
+            'mid_size' => 12,
+            'current' => ($paged ? $paged : 1),
+            'prev_text' => '&lsaquo;',
+            'next_text' => '&rsaquo;',
+          ));
+        ?>
+      </nav>
+      <?php  wp_reset_postdata(); ?>
       
     </div>
 
