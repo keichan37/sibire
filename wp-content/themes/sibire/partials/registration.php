861b4011f0cfcf2dfe544d60199a093513f420e1
Kei Abukawa <keichan37@gmail.com>
2018-06-01 10:51:14 +0900

    サービス詳細 削除

--- wp-content/themes/sibire/partials/registration.php
+++ wp-content/themes/sibire/partials/registration.php
@@ -3,9 +3,11 @@
     <div class="partials-img-wrap">
       <img class="partials-img" src="<?php echo get_template_directory_uri(); ?>/images/partials-registration/registration-service.png" alt="">
     </div>
+    <?php /* ?>
     <div class="partials-link-wrap">
       <a class="partials-link" href="/service">サービスの詳細はこちら</a>
     </div>
+    <?php */ ?>
     <a class="partials-registration" href="/registration">サービスに申し込む</a>
     <p>
       エンジニアの<br />東京にこだわらない働き方を支援
