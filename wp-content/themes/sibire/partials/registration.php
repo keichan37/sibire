a9cb98e323a5c428ec023f5ceeaf0420e187cfbf
Kei Abukawa <keichan37@gmail.com>
2018-06-01 17:50:54 +0900

    button

--- wp-content/themes/sibire/partials/registration.php
+++ wp-content/themes/sibire/partials/registration.php
@@ -1,17 +1,17 @@
 <div class="partials">
+  <?php /* ?>
   <div class="partials-registration-wrap">
-    <?php /* ?>
     <div class="partials-img-wrap">
       <img class="partials-img" src="<?php echo get_template_directory_uri(); ?>/images/partials-registration/registration-service.png" alt="">
     </div>
     <div class="partials-link-wrap">
       <a class="partials-link" href="/service">サービスの詳細はこちら</a>
     </div>
-    <?php */ ?>
     <a class="partials-registration" href="/registration">サービスに申し込む</a>
     <p>
       エンジニアの<br />東京にこだわらない働き方を支援
     </p>
     <?php get_template_part('partials/sns-share'); ?>
   </div>
+  <?php */ ?>
 </div>
