352fc5c7373388f1ca9ae9dd3c8341a437738572
Kei Abukawa <keichan37@gmail.com>
2018-06-06 15:07:37 +0900

    404

--- wp-content/themes/sibire/404.php
+++ wp-content/themes/sibire/404.php
@@ -1,16 +1,14 @@
 <?php /* Template Name: 404ページ */ ?>
 
   <?php get_header("lp"); //ヘッダーリニューアル?>
-    <div id="common">
-      <div class="container">
-        <div class="page-wrap">
-          <?php get_template_part('breadcrumb'); //パンくずリスト ?>
-          <section>
-            <h1>ページが見つかりませんでした</h1>
-            <div class="page-content">あなたがアクセスしようとしたページは削除されたかURLが変更されています。</div>
-          </section>
+    <div id="summary" class="error">
+      <div class="summary-cover">
+        <div class="container">
+          <h1>ページが見つかりませんでした</h1>
+          <div class="page-content">あなたがアクセスしようとしたページは削除されたかURLが変更されています。</div>
         </div>    
       </div>    
+      <?php get_search_form(); ?>
       
     </div>
     <?php get_footer("lp"); //フッターリニューアル?>
