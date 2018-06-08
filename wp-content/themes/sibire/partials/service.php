8971e72f26effb7054f2069c8af85cb16dee8bc7
Kei Abukawa <keichan37@gmail.com>
2018-06-08 11:15:09 +0900

    /?page_id=272

--- wp-content/themes/sibire/partials/service.php
+++ wp-content/themes/sibire/partials/service.php
@@ -1,5 +1,5 @@
 <div class="e-fixed-box"></div>
 <div id="lp" class="single-service r-fixed-box">
   <?php get_template_part('partials/title'); ?>
-  <a class="button button-small" href="/registration">サービスに申し込む</a>
+  <a class="button button-small" href="/?page_id=272">サービスに申し込む</a>
 </div>
