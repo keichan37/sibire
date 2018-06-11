99250564011b8a31077e27c2fc73bfb9d91528bd
Kei Abukawa <keichan37@gmail.com>
2018-06-11 09:22:45 +0900

    カテゴリ一覧

--- wp-content/themes/sibire/category.php
+++ wp-content/themes/sibire/category.php
@@ -1,34 +1,22 @@
-<?php /* Template Name: カテゴリページ */ ?>
-
   <?php get_header(); ?>
-    <div id="common">
-        <div class="common-cover custom-field <? echo $cat_name ?>">
-          <div class="container">
-            <?php get_template_part('breadcrumb');?>
-            <h1><?php the_title(); ?></h1>
-          </div>
-        </div>
+    <div id="summary" class="category">
+      <div class="summary-cover">
         <div class="container">
-          <div class="common-left">
-            <div class="common-grid-wrap">
-              <?php if(have_posts()): while(have_posts()):the_post(); ?>
-                <?php get_template_part('partials/common-grid'); ?>
-
-              <?php endwhile; else: //投稿が存在しない場合 ?>
-                <p>記事がありません</p>
-              <?php endif; ?>
-            </div>
-            <?php if (function_exists("pagination")) {
-              pagination($custom_query->max_num_pages);
-            } ?>
-        </div>    
-        <div class="common-right">
-          <?php get_search_form(); ?>
-          <?php get_template_part('partials/recruit-map'); ?>
-          <?php get_template_part('partials/tag'); ?>
-          <?php get_template_part('partials/registration'); ?>
+          <h1><?php the_title(); ?></h1>
+          <?php the_content(); ?>
         </div>
-      </div>    
-      
+      </div>
+      <div class="container">
+        <div class="summary-grid-wrap">
+          <?php if(have_posts()): while(have_posts()):the_post(); ?>
+            <?php get_template_part('partials/summary-grid'); ?>
+          <?php endwhile; endif; ?>
+        </div>
+        <?php if (function_exists("pagination")) {
+          pagination($custom_query->max_num_pages);
+        } ?>
+      </div>
     </div>    
-    <?php get_footer(); ?>
+      
+  </div>    
+  <?php get_footer(); ?>
