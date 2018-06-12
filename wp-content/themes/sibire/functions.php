cfd004d62f2498aeedf3978b11efad84fe18cbda
Kei Abukawa <keichan37@gmail.com>
2018-06-12 09:27:55 +0900

    smartnews 3

--- wp-content/themes/sibire/functions.php
+++ wp-content/themes/sibire/functions.php
@@ -296,7 +296,6 @@ function custom_feed_rss2(){
     load_template(get_template_directory() . $rss2_file);
 }
 add_action('do_feed_rss2', 'custom_feed_rss2', 10);
-<?php
 function mysite_feed_request($vars) {
   if ( isset( $vars['feed'] ) && !isset( $vars['post_type'] ) ) {
     $vars['post_type'] = array(
@@ -314,4 +313,3 @@ function mysite_feed_request($vars) {
 add_filter( 'request', 'mysite_feed_request' );
 
 ?>
-
