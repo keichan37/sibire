0aaa49932ebbfb14c37048b84c0c9a79c9b98169
Kei Abukawa <keichan37@gmail.com>
2018-06-26 13:41:57 +0900

    Ruby

--- wp-content/themes/sibire/searchform.php
+++ wp-content/themes/sibire/searchform.php
@@ -1,5 +1,5 @@
 <form role="search" method="get" class="searchform" action="<?php echo home_url( '/' ); ?>" autocomplete="off">
-  <input type="text" name="s" id="s" value="<?php the_search_query(); ?>" placeholder="例: システムエンジニア"/>
+  <input type="text" name="s" id="s" value="<?php the_search_query(); ?>" placeholder="例: Ruby"/>
   <div class="submit-wrap"><input type="submit" id="searchsubmit" value="検索する" /></div>
 <?php /*
 <?php wp_dropdown_categories( 'show_option_all=カテゴリ選択' ); ?>
