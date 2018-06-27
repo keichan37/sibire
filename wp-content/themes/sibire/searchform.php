fcd9d0a47a55c2e98ec2d58708ffd6f1cc580947
Kei Abukawa <keichan37@gmail.com>
2018-06-27 11:09:03 +0900

    search icon

--- wp-content/themes/sibire/searchform.php
+++ wp-content/themes/sibire/searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" class="searchform" action="<?php echo home_url( '/' ); ?>" autocomplete="off">
   <input type="text" name="s" id="s" value="<?php the_search_query(); ?>" placeholder="例: Ruby"/>
-  <div class="submit-wrap"><input type="submit" id="searchsubmit" value="検索する" /></div>
+  <div class="submit-wrap"><input type="submit" id="searchsubmit" value="&#xf10d;" /></div>
 <?php /*
 <?php wp_dropdown_categories( 'show_option_all=カテゴリ選択' ); ?>
 <?php $tags = get_tags(); if ( $tags ) : ?>
