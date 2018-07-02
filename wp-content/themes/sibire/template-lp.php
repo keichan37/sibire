bbb3c78c44169adc2376945089261ca65d4b0b53
Kei Abukawa <keichan37@gmail.com>
2018-07-02 13:45:45 +0900

    voice

--- wp-content/themes/sibire/template-lp.php
+++ wp-content/themes/sibire/template-lp.php
@@ -84,7 +84,21 @@
         <?php query_posts( $args ); ?>
           <div class="topic-summary voice">
         <?php while (have_posts()) : the_post(); ?>
-          <?php get_template_part('partials/lp-grid'); ?>
+          <?php
+            $thumbnail_id = get_post_thumbnail_id();
+            $thumbnail_url = wp_get_attachment_image_src($thumbnail_id,'medium', true);
+          ?>
+          <a class="single-related" href="<?php the_permalink(); ?>">
+            <?php if (has_post_thumbnail()): ?>
+              <img class="single-related-eyecatch owl-lazy" data-src="<?php echo $thumbnail_url[0]; ?>" />
+            <?php else: ?>
+              <img class="single-related-eyecatch owl-lazy" src="<?php echo get_template_directory_uri(); ?>/images/dummy.png" />
+            <?php endif; ?>
+
+            <h5><?php the_title(); ?></h5>
+            <p><?php echo nl2br(get_post_meta($post->ID, 'subtitle', true)); ?></p>
+          </a>
+
         <?php endwhile; ?>
           </div>
 
