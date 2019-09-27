bd003e0c0c293ce29b8e7742786b104305d76929
Kei Abukawa <keichan37@gmail.com>
2019-09-27 09:41:22 +0900

    東京

--- wp-content/themes/sibire/single-recruit-event.php
+++ wp-content/themes/sibire/single-recruit-event.php
@@ -81,17 +81,7 @@
           <div class="single-wrap">
             <div class="single">
               <div class="single-left">
-                <a href="
-                  <?php
-                    $event = get_field('event');
-                    if ($event == 'tokyo'){
-                      echo '/wakayama_draft_tokyo';
-                    }else{
-                      echo '/wakayama_draft_osaka';
-                    }
-                  ?>">
-                  <img class="single-title-img" src="<?php echo get_template_directory_uri(); ?>/images/template-lovedraft/single-title.png" alt="Wakayama Love ドラフト">
-                </a>
+                <a href="<?php echo get_permalink(48992); ?>"><img class="single-title-img" src="<?php echo get_template_directory_uri(); ?>/images/template-lovedraft/single-title.png" alt="Wakayama Love ドラフト"></a>
                 <?php while(have_posts()): the_post(); ?>
                   <article>
                     <h1 class="single-title"><?php the_title(); ?></h1>
