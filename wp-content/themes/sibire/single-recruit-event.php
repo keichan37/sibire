34f7dffc8299f9d70baf9bdd8e94b1c0d6dc0160
Kei Abukawa <keichan37@gmail.com>
2020-06-04 13:51:13 +0900

    online

--- wp-content/themes/sibire/single-recruit-event.php
+++ wp-content/themes/sibire/single-recruit-event.php
@@ -81,9 +81,13 @@
           <div class="single-wrap">
             <div class="single">
               <div class="single-left">
-                <? $txt = get_field('event'); if($txt){ ?>
-                <? echo $txt; ?>
-                <? } ?>
+                <?php
+                  $colors = get_field('event');
+                  if( $colors && in_array('event', $colors ) ) {
+                      echo 'online';
+                  }
+                ?>
+
                 <?php if(get_field('event') == "online"): ?>
                   <a href="<?php echo get_permalink(57932); ?>"><img class="single-title-img" src="<?php echo get_template_directory_uri(); ?>/images/template-lovedraft/single-online-title.jpg" alt="Wakayama Love ドラフト"></a>
                 <?php else: ?>
