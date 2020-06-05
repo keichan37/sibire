4751db91b1762958fce2b5522d1521f2b47a162b
Kei Abukawa <keichan37@gmail.com>
2020-06-05 10:27:55 +0900

    追加修正

--- wp-content/themes/sibire/single-recruit-event.php
+++ wp-content/themes/sibire/single-recruit-event.php
@@ -85,7 +85,7 @@
                 <?php $cus = get_field('event'); ?>
                 <? if($cus && in_array('online', $cus )): ?>
                   <a href="<?php echo get_permalink(57932); ?>"><img class="single-title-img" src="<?php echo get_template_directory_uri(); ?>/images/template-lovedraft/single-online-title.jpg" alt="Wakayama Love ドラフト"></a>'
-                <?php else if($cus && in_array('online', $cus )): ?>
+                <?php else: ?>
                   <a href="<?php echo get_permalink(48992); ?>"><img class="single-title-img" src="<?php echo get_template_directory_uri(); ?>/images/template-lovedraft/single-title.png" alt="Wakayama Love ドラフト"></a>
                 <?php endif; ?>
 
