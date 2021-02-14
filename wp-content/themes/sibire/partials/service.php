e407b6e0f0b316eda53dc8d986396e9e528227ae
Kei Abukawa <keichan37@gmail.com>
2021-02-14 20:09:24 +0900

    確認

--- wp-content/themes/sibire/partials/service.php
+++ wp-content/themes/sibire/partials/service.php
@@ -1,5 +1,5 @@
 <div id="lp" class="single-service">
   この求人について詳しく聞く
-  <a class="button button-small" href="https://timelex.com/?title=<?php the_ID(); ?>" target="_blank" rel="nofollow">オンラインWeb面談に申し込み</a>
+  <a class="button button-small" href="https://timelex.com/<?php if ( in_array(get_post_type(), array('recruit')) ): ?><? $txt = get_field('subtitle'); if($txt){ ?>/?JOBTITLE=<? echo $txt; ?><? } ?>&JOBID=<?php the_ID(); ?>" target="_blank" rel="nofollow">オンラインWeb面談に申し込み</a>
   登録1分！日にちを選んでエントリー
 </div>
